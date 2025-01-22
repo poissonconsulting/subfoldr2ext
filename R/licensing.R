c27872274878c9bf9ed2b8cf28a029f0699dde37
Duncan Kennedy <duncan@poissonconsulting.ca>
2025-01-22 10:42:43 -0700

    styler

--- R/licensing.R
+++ R/licensing.R
@@ -1,15 +1,15 @@
 licensing_md <- function() {
   "## Licensing
-  
-  Enter liscencing information in the licensing.R file within the licensing_md()
-  reference software dev book license section. 
 
-For example:  
+  Enter liscencing information in the licensing.R file within the licensing_md()
+  reference software dev book license section.
+
+For example:
   Copyright XXXX The Entity\\
-  
+
   The documentation is released under the
   XXXX
-  
+
   The code is released under the
   XXXX
 "
